5176d9aae38951a62dfe652bf61f7aa8d663a769
Lukmanudhin M <lukmanudhinm@gmail.com>
2024-02-17 13:25:30 +0400

    Student Grade

--- src/StudentsMark.java
+++ src/StudentsMark.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class StudentsMark {
+import java.util.Scanner;
+public class StudentsMark {
+   public static void main(String[] args) {
+       Scanner input=new Scanner(System.in);
+       System.out.println("Enter Your Marks: ");
+       int num=input.nextInt(); if (num>=85 && num<=100) {
+           System.out.println("Congratulations You Scored Grade A"); }
+       else if (num>=60 && num<=85) {
+           System.out.println("You Scored Grade B"); }
+       else if (num>=40 && num<=60) {
+           System.out.println("You Scored Grade C"); }
+       else{ System.out.println("You are Fail");
+       }
+   }
 }
+
